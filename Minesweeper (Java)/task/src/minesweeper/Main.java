6e588b2288bd43ed2e27c44e1c7cf9009be66a24
Trayana Boykova <trayana.boykova@gmail.com>
2025-11-27 14:32:55 +0200

    completed stage 1

--- Minesweeper (Java)/task/src/minesweeper/Main.java
+++ Minesweeper (Java)/task/src/minesweeper/Main.java
@@ -2,6 +2,15 @@ package minesweeper;
 
 public class Main {
     public static void main(String[] args) {
-        // write your code here
+        // A sample 9x9 minefield: 'X' = mine, '.' = safe cell
+        System.out.println(".X.......");
+        System.out.println(".....X..X");
+        System.out.println("....X....");
+        System.out.println("......X..");
+        System.out.println("..X......");
+        System.out.println("....X....");
+        System.out.println("..X......");
+        System.out.println("..X......");
+        System.out.println("......X..");
     }
 }
